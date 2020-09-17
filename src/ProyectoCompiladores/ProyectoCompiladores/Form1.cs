f99f881b0da77310c49d0430b14b0429062b0c6a
Pedejeca135 <pjco90@hotmail.com>
2020-09-16 23:20:34 -0500

    ya convierte a posfija parcialmente bien, lets fix it

--- src/ProyectoCompiladores/ProyectoCompiladores/Form1.cs
+++ src/ProyectoCompiladores/ProyectoCompiladores/Form1.cs
@@ -43,147 +43,198 @@ namespace ProyectoCompiladores
         {
             TB_SubirArchivo.Text = "";
         }
-        
-/* * *
- while ( no ocurra un error && no sea fin de la expresión infija )
-{
- switch ( carácter )
- {
-    Paréntesis izquierdo:
-        Insertar en la pila;
-    break;
-    Paréntesis derecho:
-        Extraer de la pila y desplegar en posfija hasta encontrar “paréntesis izquierdo” (no desplegarlo);
-    break;
-    Operando:
-        Desplegar en posfija.
-    break;
-    Operador:
-    band = true;
-    while ( band )
-    {
-        if ( la pila está vacía || el tope de la pila es un “paréntesis izquierdo” ||
-        el operador tiene mayor prioridad que el tope de la pila )
-        {
-            Insertar el operador en la pila.
-            band = false;
-        }
-        else
-        {
-            Extraer el tope de la pila y desplegar en posfija.
-        }
-        break;
-    }
-    Apuntar al siguiente carácter de la expresión infija.
-    }
-    Extraer y desplegar en posfija los elementos de la pila hasta que se vacíe.
-}
-*/
 
-/**************************
-
-EVALUACIÓN DE EXPRESIONES EN NOTACIÓN POSFIJA.
-
-1. Inicializar una pila.
-2. Apuntar al primer carácter de la expresión posfija.
-while ( no ocurra un error && no sea fin de la expresión posfija )
-{
-    switch ( carácter )
-    {
-        Operando:
-        Insertar en la pila;
-        break;
-        Operador:
-        if ( si es “unario”)
+        /* * *
+         while ( no ocurra un error && no sea fin de la expresión infija )
         {
-            Extraer el valor del tope de la pila y aplicar el operador.
-            (Se produce un error en caso de no tener valor).
-            Insertar el resultado en el nuevo tope de la pila.
+         switch ( carácter )
+         {
+            Paréntesis izquierdo:
+                Insertar en la pila;
+            break;
+            Paréntesis derecho:
+                Extraer de la pila y desplegar en posfija hasta encontrar “paréntesis izquierdo” (no desplegarlo);
+            break;
+            Operando:
+                Desplegar en posfija.
+            break;
+            Operador:
+            band = true;
+            while ( band )
+            {
+                if ( la pila está vacía || el tope de la pila es un “paréntesis izquierdo” ||
+                el operador tiene mayor prioridad que el tope de la pila )
+                {
+                    Insertar el operador en la pila.
+                    band = false;
+                }
+                else
+                {
+                    Extraer el tope de la pila y desplegar en posfija.
+                }
+                break;
+            }
+            Apuntar al siguiente carácter de la expresión infija.
+            }
+            Extraer y desplegar en posfija los elementos de la pila hasta que se vacíe.
         }
-        else ( si es “binario”)
-        {
-            Extraer los 2 valores del tope de la pila y aplicar el operador.
-            (Se produce un error en caso de no tener los 2 valores).
-            Insertar el resultado en el nuevo tope de la pila.
-        }
-        break;
-    }
-    Apuntar al siguiente carácter de la expresión posfija.
-}
+        */
 
-         * */
+        /**************************
+
+        EVALUACIÓN DE EXPRESIONES EN NOTACIÓN POSFIJA.
+
+        1. Inicializar una pila.
+        2. Apuntar al primer carácter de la expresión posfija.
+        while ( no ocurra un error && no sea fin de la expresión posfija )
+        {
+            switch ( carácter )
+            {
+                Operando:
+                Insertar en la pila;
+                break;
+                Operador:
+                if ( si es “unario”)
+                {
+                    Extraer el valor del tope de la pila y aplicar el operador.
+                    (Se produce un error en caso de no tener valor).
+                    Insertar el resultado en el nuevo tope de la pila.
+                }
+                else ( si es “binario”)
+                {
+                    Extraer los 2 valores del tope de la pila y aplicar el operador.
+                    (Se produce un error en caso de no tener los 2 valores).
+                    Insertar el resultado en el nuevo tope de la pila.
+                }
+                break;
+            }
+            Apuntar al siguiente carácter de la expresión posfija.
+        }
+
+                 * */
+
+        static String alfabeto = "abcdefghijklmnñopqrstuvxyz0123456789";
+        static String op_Presedecia1 = "*+?";//jerarquia 1
+        static String op_Presedecia2 = "&";//jerarquia 2
+        static String op_Presedecia3 = "|";//jerarquia 3
+        static String op = op_Presedecia1 + op_Presedecia2 + op_Presedecia3;
+
         private void InToPosBoton_Click(object sender, EventArgs e)
         {
-            String alfabeto = "abcdefghijklmnñopqrstuvxyz0123456789";
-            op_Presedecia1 = "*+?";//jerarquia 1
-            op_Presedecia2 = "&";//jerarquia 2
-            op_Presedecia3 = "|";//jerarquia 3
+            String posFija = "";
+
+            Stack<char> pila = new Stack<char>();
+
 
             String infija = inFijaTextBox.Text;
-            infija = desglosaCorchetes(infija);
+            //infija = desglosaCorchetes(infija);
 
             //variables de control para el primer while
             Boolean error = false;
             int contadorInfija = 0;
+            Boolean band = false;
 
-            if(infija = "")
+            if (infija == "")
             {
                 error = true;
             }
 
             while (!error && contadorInfija <= infija.Length)
             {
-                String caracter = infija.Substring(contadorInfija, 1);
+                //char caracter = infija.Substring(contadorInfija, 1);
+                char caracter = infija[contadorInfija];
+
                 switch (caracter)
                 {
-                    case "(":
-                        //
+                    case '(':
+                        pila.Push(caracter);
                         break;
-                    case ")":
-                        //
+                    case ')':
+                        while (pila.Peek() != '(')
+                        {
+                            posFija += pila.Pop(); ;//despliega en posFija
+                        }
                         break;
                     default:
-                        if(alfabeto.Contains(caracter)|| )
+                        if (alfabeto.Contains(caracter))//es un operando.
                         {
-                           
+                            posFija += caracter;//despliega en posFija
                         }
-                        else//se introdujo algun caracter invalido.
+                        else
                         {
-                            error = true;
+                            if (op.Contains(caracter))//es un operador.
+                            {
+                                band = true;
+                                while (band)
+                                {
+                                    if (!pila.Any() || pila.Peek() == '(' || prioridad(caracter, pila.Peek()))
+                                    {
+                                        pila.Push(caracter);
+                                        band = false;
+                                    }
+                                    else
+                                    {
+                                        posFija += pila.Pop();
+                                    }
+                                }
+                            }
+                            else//se introdujo algun caracter invalido.
+                            {
+                                error = true;
+                            }
                         }
-                        break;                  
+                        break;
                 }
-                Boolean bandera = true
-                while(bandera)
-                {
-
-                }                  
-
+                contadorInfija++;
             }
+            posFijaTextBox.Text = posFija;
         }
 
+        private Boolean prioridad(char mayor, char menor)
+        {
+            if (mayor == menor || op_Presedecia1.Contains(menor))
+            {
+                return false;
+            }
+            if (op_Presedecia1.Contains(mayor) && !op_Presedecia1.Contains(menor))
+            {
+                return true;
+            }
+            if (op_Presedecia2.Contains(mayor) && !op_Presedecia2.Contains(menor))
+            {
+                return true;
+            }
+            if (op_Presedecia3.Contains(menor))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /*
         private String desglosaCorchetes(String infija)
         {
             String res;
 
-            List<int> corchetesIniciales = List<int>();
-            List<int> corchetesFinales = List<int>();
+            List<int> corchetesIniciales = new List<int>();
+            List<int> corchetesFinales = new List<int>();
 
             Boolean salir = false;
             for (int i = s.IndexOf('['); i > -1; i = s.IndexOf('[', i + 1))
             {
-                    corchetesIniciales.Add(i);
+                corchetesIniciales.Add(i);
             }
             for (int i = s.IndexOf(']'); i > -1; i = s.IndexOf(']', i + 1))
             {
-                    corchetesFinales.Add(i);
+                corchetesFinales.Add(i);
             }
-            
-            if(corchetesFinales.Count() != corchetesFinales.Count())
+
+            if (corchetesFinales.Count() != corchetesFinales.Count())
             {
                 return "";
-            }            
-        
+            }
+
         }
+        */
     }//Forms END
 }//namespace END
