099423e429549ce9280d317b74e2e54a2b005176
Pedejeca135 <pjco90@hotmail.com>
2020-10-03 11:34:36 -0500

    AFN creacion1

--- src/ProyectoCompiladores/ProyectoCompiladores/AFN.cs
+++ src/ProyectoCompiladores/ProyectoCompiladores/AFN.cs
@@ -8,24 +8,53 @@ namespace ProyectoCompiladores
 {
     public class AFN
     {
-        List<Estado> estados;
+        protected Estado inicial = null;
+        protected Estado final = null;
+
+        protected List<Estado> estados;
+        protected String op = "*+?&|";
+        protected String alfabeto = "";
+        
 
         public AFN(String expresionPosfija)
         {
             estados = new List<Estado>();
-
-            
-
+            alfabeto = encuentraAlfabeto(expresionPosfija) + "ε";
         }
 
+        //para determinar el alfabeto de la expresion
+        public String encuentraAlfabeto(String expresionPosfija)
+        {
+            String res = "";
+            foreach(char caracter in expresionPosfija)
+            {
+                if(!op.Contains(caracter))
+                {
+                    res += caracter;
+                }
+            }
+            return res;
+        }
+
+        //este metodo creara el AFN mientras hace la evaluacion de la expresion posfija.
         public void algoritmoDeEvaluacion(String expresionPosfija)
         {
 
         }
 
+        /*agrega un nuevo estado de inicio y uno de aceptacion con una transicion*/
         public void addEstado()
         {
-
+            if(inicial == null && final == null)
+            {
+                inicial = new Estado(0,alfabeto);
+                final = new Estado(1,alfabeto);
+            }
+            else
+            {
+                 inicial = new Estado(final.)
+            }
+            //Estado inicial = new Estado(,);
         }
 
         /*Agrega un nuevo estado final y un nuevo estado inicial*/
--- src/ProyectoCompiladores/ProyectoCompiladores/Estado.cs
+++ src/ProyectoCompiladores/ProyectoCompiladores/Estado.cs
@@ -8,9 +8,33 @@ namespace ProyectoCompiladores
 {
     public class Estado
     {
-        public Estado()
-        {
+        int index;
+        int tipo;//1->Inicial, 2->Transision, 3->Aceptacion
 
+        protected List<List<Estado>> fila;
+
+        public Estado(int index, String alfabeto)
+        {
+            this.index = index;
+            fila = new List<List<Estado>>(alfabeto.Length);
+            
         }
+
+        public void sumaUnoAlIndice()
+        {
+            index++;
+        }
+
+        public void cambiaTipo(int newTipo)
+        {
+            tipo = newTipo;
+        }
+
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+        
     }
 }
