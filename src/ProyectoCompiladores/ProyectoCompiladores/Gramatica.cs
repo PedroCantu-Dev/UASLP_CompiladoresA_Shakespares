16322143397c42ca04ec7b8bb6f0d69e1268026d
pedro cantu <pjco90@hotmail.com>
2020-11-28 18:26:33 -0600

    Primera reunion avance 6

---
+++ src/ProyectoCompiladores/ProyectoCompiladores/Gramatica.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCompiladores
+{
+
+   
+
+    public class Gramatica
+    {
+        List<String> terminales;
+        List<String> NoTerminales
+
+
+        public Gramatica(List<String> terminales, List<String> NoTerminales)
+        {
+            this.terminales = terminales;
+            this.NoTerminales = NoTerminales;
+        }
+    }
+
+}
